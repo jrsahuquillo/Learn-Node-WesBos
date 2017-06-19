1df618e8c52e6eae7cea9b40c8fc68a310438cf2
jrsahuquillo <jrsahuquillo@gmail.com>
2017-06-19 04:41:19 +0200

    Module 13: Advanced Relationships + Aggregations

--- models/Store.js
+++ models/Store.js
@@ -37,7 +37,10 @@ const storeSchema = new mongoose.Schema({
     type: mongoose.Schema.ObjectId,
     ref: 'User',
     required: 'You must supply an author'
-  }
+  },
+}, {
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true }
 });
 
 // Define our indexes
@@ -67,23 +70,47 @@ storeSchema.pre('save', async function(next) {
 });
 
 storeSchema.statics.getTagsList = function() {
-  return this.aggregate([{
-      $unwind: '$tags'
-    },
-    {
-      $group: {
-        _id: '$tags',
-        count:  {
-          $sum: 1
-        }
-      }
-    },
-    {
-      $sort: {
-        count: -1
-      }
-    }
+  return this.aggregate([
+    { $unwind: '$tags' },
+    { $group: { _id: '$tags', count:  { $sum: 1 } } },
+    { $sort: { count: -1 } }
+  ]);
+};
+
+storeSchema.statics.getTopStores = function() {
+  return this.aggregate([
+    //Lookup Stores and populate their reviews
+    { $lookup: { from: 'reviews', localField: '_id', foreignField: 'store', as: 'reviews' } },
+    //Filter for only items that have 2 or more reviews
+    { $match: { 'reviews.1': { $exists: true} } },
+    //Add the average reviews field
+    { $project: {
+      photo: '$$ROOT.photo',
+      name: '$$ROOT.name',
+      reviews: '$$ROOT.reviews',
+      slug: '$$ROOT.slug',
+      averageRating: { $avg: '$reviews.rating' }
+    } },
+    //Sort it by our new field, highest reviews first
+    { $sort: { averageRating: -1 }},
+    //Limit to at most 10
+    { $limit: 10 }
   ]);
 }
+
+// find reviews where the stores_id property === reviews store property
+storeSchema.virtual('reviews', {
+  ref: 'Review', // what model to link?
+  localField: '_id', // which field on the store?
+  foreignField: 'store' // which field on the review?
+});
+
+function autopopulate(next) {
+  this.populate('reviews');
+  next();
+};
+
+storeSchema.pre('find', autopopulate);
+storeSchema.pre('findOne', autopopulate);
 
 module.exports = mongoose.model('Store', storeSchema);
